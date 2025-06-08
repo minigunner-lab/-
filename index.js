c2b6f97693f3760e9476930b8980734db1436710
minigunner-lab <invincibleass@outlook.com>
2025-06-08 13:17:09 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-fetch('https://gentle-surf-870c.fusion-minded-6f.workers.dev', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    ip: await fetch('https://api.ipify.org?format=text').then(r => r.text())
-  })
-});
+(async () => {
+  try {
+    const ip = await fetch('https://api.ipify.org?format=text').then(r => r.text());
+    await fetch('https://gentle-surf-870c.fusion-minded-6f.workers.dev', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ ip })
+    });
+  } catch {}
+})();
